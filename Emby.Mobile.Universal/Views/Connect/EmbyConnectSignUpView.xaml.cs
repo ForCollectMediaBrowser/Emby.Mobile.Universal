63e5c606dac910abe9b82c2bb7b3c6bdacd76247
Scott Lovegrove <scottisafool@live.co.uk>
2015-08-25 11:12:27 +0100

    Added properties/methods to VM

--- Emby.Mobile.Universal/Views/Connect/EmbyConnectSignUpView.xaml.cs
+++ Emby.Mobile.Universal/Views/Connect/EmbyConnectSignUpView.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace Emby.Mobile.Universal.Views.Connect
+﻿using Emby.Mobile.ViewModels;
+
+namespace Emby.Mobile.Universal.Views.Connect
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
@@ -9,5 +11,7 @@
         {
             this.InitializeComponent();
         }
+
+        private EmbyConnectSignUpViewModel SignUp => DataContext as EmbyConnectSignUpViewModel;
     }
 }
