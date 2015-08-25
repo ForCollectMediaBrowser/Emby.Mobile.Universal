9b7e0da223bb371353759da6fc23b0137731c17f
Scott Lovegrove <scottisafool@live.co.uk>
2015-08-25 09:54:18 +0100

    Added Sign up view model (some minor code style changes)

---
+++ Emby.Mobile.ViewModels/ViewModels/EmbyConnectSignUpViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Emby.Mobile.Core.Interfaces;
+
+namespace Emby.Mobile.ViewModels
+{
+    public class EmbyConnectSignUpViewModel : PageViewModelBase
+    {
+        public EmbyConnectSignUpViewModel(IServices services) : base(services)
+        {
+        }
+    }
+}
