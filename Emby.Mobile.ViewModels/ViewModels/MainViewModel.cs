59cd84a4b8da0f34d466968ee716dfed4e5d935e
Scott Lovegrove <scottisafool@live.co.uk>
2015-08-26 18:34:33 +0100

    User can sign out now

--- Emby.Mobile.ViewModels/ViewModels/MainViewModel.cs
+++ Emby.Mobile.ViewModels/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Emby.Mobile.Core.Interfaces;
+using GalaSoft.MvvmLight.Command;
 
 namespace Emby.Mobile.ViewModels
 {
@@ -9,5 +10,29 @@ namespace Emby.Mobile.ViewModels
         }
 
         public string ConnectedTo => string.Format(GetLocalizedString("LabelServerConnected"), Services.ServerInfo?.ServerInfo?.Name);
+
+        public RelayCommand SignOutCommand
+        {
+            get
+            {
+                return new RelayCommand(async () =>
+                {
+                    var signedInWithConnect = AuthenticationService.SignedInUsingConnect;
+                    if (await AuthenticationService.SignOut())
+                    {
+                        if (signedInWithConnect)
+                        {
+                            Services.NavigationService.NavigateToEmbyConnect();
+                        }
+                        else
+                        {
+                            Services.NavigationService.NavigateToChooseProfile();
+                        }
+
+                        Services.NavigationService.ClearBackStack();
+                    }
+                });
+            }
+        }
     }
 }
