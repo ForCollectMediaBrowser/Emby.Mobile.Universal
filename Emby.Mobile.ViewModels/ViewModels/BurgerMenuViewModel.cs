18d6e66dbaa794105f022aa4d0cbb09215c23d57
Scott Lovegrove <scottisafool@live.co.uk>
2015-08-30 21:23:45 +0100

    Displays the list of views in the burger menu

--- Emby.Mobile.ViewModels/ViewModels/BurgerMenuViewModel.cs
+++ Emby.Mobile.ViewModels/ViewModels/BurgerMenuViewModel.cs
@@ -1,12 +1,20 @@
-﻿using Emby.Mobile.Core.Interfaces;
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Cimbalino.Toolkit.Extensions;
+using Emby.Mobile.Core.Extensions;
+using Emby.Mobile.Core.Interfaces;
 using Emby.Mobile.Universal.Core.Helpers;
 using Emby.Mobile.ViewModels.Entities;
 using GalaSoft.MvvmLight.Command;
+using MediaBrowser.Model.Net;
 
 namespace Emby.Mobile.ViewModels
 {
     public class BurgerMenuViewModel : ViewModelBase
     {
+        private bool _viewsLoaded;
+
         public BurgerMenuViewModel(IServices services) : base(services)
         {
             if (!IsInDesignMode)
@@ -15,30 +23,13 @@ namespace Emby.Mobile.ViewModels
                 {
                     SetUsernameAndProfilePicture();
                 };
+
+                Views = new ObservableCollection<ItemViewModel>();
             }
         }
 
-        public void Start()
-        {
-            SetUsernameAndProfilePicture();
-        }
-
-        private void SetUsernameAndProfilePicture()
-        {
-            Services.Dispatcher.RunAsync(() =>
-            {
-                if (!AuthenticationService.SignedInUsingConnect && !AuthenticationService.IsSignedIn)
-                {
-                    User = null;
-                }
-                else
-                {
-                    User = new UserDtoViewModel(Services, AuthenticationService.SignedInUser);
-                }
-            });
-        }
-
         public UserDtoViewModel User { get; set; }
+        public ObservableCollection<ItemViewModel> Views { get; set; }
 
         public RelayCommand NavigateToSettingsCommand
         {
@@ -59,6 +50,51 @@ namespace Emby.Mobile.ViewModels
                 {
                     await SignOutHelper.SignOut(Services);
                 });
+            }
+        }
+
+        public void Start()
+        {
+            SetUsernameAndProfilePicture();
+            LoadViews(false).ConfigureAwait(false);
+        }
+
+        private void SetUsernameAndProfilePicture()
+        {
+            Services.Dispatcher.RunAsync(() =>
+            {
+                if (!AuthenticationService.SignedInUsingConnect && !AuthenticationService.IsSignedIn)
+                {
+                    User = null;
+                }
+                else
+                {
+                    User = new UserDtoViewModel(Services, AuthenticationService.SignedInUser);
+                }
+            });
+        }
+
+        private async Task LoadViews(bool isRefresh)
+        {
+            if (_viewsLoaded && !isRefresh)
+            {
+                return;
+            }
+
+            try
+            {
+                var item = await ApiClient.GetUserViews(AuthenticationService.SignedInUserId);
+                if (item != null && !item.Items.IsNullOrEmpty())
+                {
+                    Views.Clear();
+                    var views = item.Items.Select(x => new ItemViewModel(Services, x)).ToObservableCollection();
+
+                    Views = views;
+                }
+            }
+            catch (HttpException ex)
+            {
+                
             }
         }
     }
--- Emby.Mobile.ViewModels/ViewModels/Entities/ItemViewModel.cs
+++ Emby.Mobile.ViewModels/ViewModels/Entities/ItemViewModel.cs
@@ -1,11 +1,29 @@
 ﻿using Emby.Mobile.Core.Interfaces;
+using GalaSoft.MvvmLight.Command;
+using MediaBrowser.Model.Dto;
 
 namespace Emby.Mobile.ViewModels.Entities
 {
     public class ItemViewModel : ViewModelBase
     {
-        public ItemViewModel(IServices services) : base(services)
+        public ItemViewModel(IServices services, BaseItemDto itemInfo) : base(services)
         {
+            ItemInfo = itemInfo;
+        }
+
+        public string Name => ItemInfo?.Name;
+
+        public BaseItemDto ItemInfo { get; set; }
+
+        public RelayCommand NavigateToItem
+        {
+            get
+            {
+                return new RelayCommand(() =>
+                {
+                    
+                });
+            }
         }
     }
 }
