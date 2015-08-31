29e6e47d63fee4270062ac492f2b904a97fb8544
7illusions <z@7illusions.com>
2015-08-31 21:49:56 +0200

    Added SystemTray service

---
+++ Emby.Mobile.Core/Interfaces/IStatusBarService.cs
@@ -0,0 +1,11 @@
+﻿namespace Emby.Mobile.Core.Interfaces
+{
+    public interface IStatusBarService
+    {
+        void DisplayIndeterminateStatus(string message);
+        void DisplayMessage(string message);
+        void DisplayError(string message);
+        void DisplayWarning(string message);
+        void DisplaySuccess(string message);
+    }
+}
---
+++ Emby.Mobile.Universal/Services/StatusBarService.cs
@@ -0,0 +1,40 @@
+﻿using Emby.Mobile.Core.Interfaces;
+using Emby.Mobile.Universal.Controls;
+using Emby.Mobile.Universal.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emby.Mobile.Universal.Services
+{
+    public class StatusBarService : IStatusBarService
+    {
+        public void DisplayError(string message)
+        {
+            SystemTrayControl.GetForCurrentView()?.DisplayProgress(message, StatusType.Error);
+        }
+
+
+        public void DisplayMessage(string message)
+        {
+            SystemTrayControl.GetForCurrentView()?.DisplayProgress(message, StatusType.Message);
+        }
+
+        public void DisplayIndeterminateStatus(string message)
+        {
+            SystemTrayControl.GetForCurrentView()?.DisplayProgress(message, StatusType.Status);
+        }
+
+        public void DisplaySuccess(string message)
+        {
+            SystemTrayControl.GetForCurrentView()?.DisplayProgress(message, StatusType.Success);
+        }
+
+        public void DisplayWarning(string message)
+        {
+            SystemTrayControl.GetForCurrentView()?.DisplayProgress(message, StatusType.Warning);
+        }
+    }
+}
