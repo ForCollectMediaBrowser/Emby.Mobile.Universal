d74b37b4b625aab699a06bdb0d20a7796c00ce38
Scott Lovegrove <scottisafool@live.co.uk>
2015-09-04 18:37:04 +0100

    Added viewmodel and data sourcing

---
+++ Emby.Mobile.ViewModels/ViewModels/Entities/UserViewViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Emby.Mobile.Core.Interfaces;
+using MediaBrowser.Model.Dto;
+
+namespace Emby.Mobile.ViewModels.Entities
+{
+    public class UserViewViewModel : ItemViewModel
+    {
+        public UserViewViewModel(IServices services, BaseItemDto itemInfo) : base(services, itemInfo)
+        {
+        }
+    }
+}
--- Emby.Mobile.ViewModels/ViewModels/MainViewModel.cs
+++ Emby.Mobile.ViewModels/ViewModels/MainViewModel.cs
@@ -1,17 +1,40 @@
-﻿using Emby.Mobile.Core.Interfaces;
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Cimbalino.Toolkit.Extensions;
+using Emby.Mobile.Core.Extensions;
+using Emby.Mobile.Core.Interfaces;
 using Emby.Mobile.Core.Strings;
 using Emby.Mobile.Universal.Core.Helpers;
+using Emby.Mobile.ViewModels.Entities;
 using GalaSoft.MvvmLight.Command;
+using MediaBrowser.Model.Net;
 
 namespace Emby.Mobile.ViewModels
 {
     public class MainViewModel : PageViewModelBase
     {
+        private bool _viewsLoaded;
+
         public MainViewModel(IServices services) : base(services)
         {
+            UserViews = new ObservableCollection<UserViewViewModel>();
         }
 
         public string ConnectedTo => string.Format(Resources.LabelServerConnected, Services.ServerInfo?.ServerInfo?.Name);
+
+        public ObservableCollection<UserViewViewModel> UserViews { get; set; }
+
+        public RelayCommand RefreshCommand
+        {
+            get
+            {
+                return new RelayCommand(async () =>
+                {
+                    await LoadData(true);
+                });
+            }
+        }
 
         public RelayCommand SignOutCommand
         {
@@ -21,6 +44,37 @@ namespace Emby.Mobile.ViewModels
                 {
                     await SignOutHelper.SignOut(Services);
                 });
+            }
+        }
+
+        protected override async Task PageLoaded()
+        {
+            await LoadData(false);
+            await base.PageLoaded();
+        }
+
+        private async Task LoadData(bool isRefresh)
+        {
+            if (_viewsLoaded && !isRefresh)
+            {
+                return;
+            }
+
+            try
+            {
+                var item = await ApiClient.GetUserViews(AuthenticationService.SignedInUserId);
+                if (item != null && !item.Items.IsNullOrEmpty())
+                {
+                    UserViews.Clear();
+                    var items = item.Items.Select(x => new UserViewViewModel(Services, x)).ToObservableCollection();
+
+                    UserViews = items;
+                    _viewsLoaded = UserViews.Any();
+                }
+            }
+            catch (HttpException ex)
+            {
+                //Utils.HandleHttpException("GetUserViews()", ex, NavigationService, Log);
             }
         }
     }
