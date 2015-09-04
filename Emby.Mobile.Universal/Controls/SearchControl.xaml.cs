268280998cbc3647f1b06f79f2b11a7fb209b525
7illusions <z@7illusions.com>
2015-09-04 14:12:29 +0200

    Added VM's and Controls

---
+++ Emby.Mobile.Universal/Controls/SearchControl.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Emby.Mobile.ViewModels;
+using Windows.UI.Xaml.Controls;
+
+namespace Emby.Mobile.Universal.Controls
+{
+    public sealed partial class SearchControl : UserControl
+    {
+        public SearchViewModel Search => DataContext as SearchViewModel;
+
+        public SearchControl()
+        {
+            InitializeComponent();
+        }
+
+        private void SearchHintControl_Tapped(object sender, Windows.UI.Xaml.Input.TappedRoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Emby.Mobile.ViewModels/ViewModels/SearchViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.ObjectModel;
+using Cimbalino.Toolkit.Extensions;
+using Emby.Mobile.Core.Extensions;
+using Emby.Mobile.Core.Interfaces;
+using MediaBrowser.Model.Net;
+using MediaBrowser.Model.Search;
+using Emby.Mobile.Core.Strings;
+using Emby.Mobile.ViewModels.Entities;
+using System.Linq;
+
+namespace Emby.Mobile.ViewModels
+{
+    public class SearchViewModel : ViewModelBase
+    {
+        public SearchViewModel(IServices services) : base(services)
+        {
+            if (!IsInDesignMode)
+            {
+
+                SearchResults = new ObservableCollection<SearchHintViewModel>();
+            }
+            else
+            {
+                SearchResults = new ObservableCollection<SearchHintViewModel>
+                {
+                    new SearchHintViewModel(services, new SearchHint { Name = "Result 1" }),
+                    new SearchHintViewModel(services,new SearchHint { Name = "Result 2" })
+                };
+            }
+        }
+
+        public string SearchText { get; set; }
+        public ObservableCollection<SearchHintViewModel> SearchResults { get; set; }
+
+        private async void OnSearchTextChanged()
+        {
+            if (SearchText?.Length > 1)
+            {
+                var query = new SearchQuery
+                {
+                    UserId = AuthenticationService.SignedInUserId,
+                    Limit = 20,
+                    StartIndex = 0,
+                    IncludeArtists = true,
+                    IncludeGenres = true,
+                    IncludeMedia = true,
+                    IncludePeople = true,
+                    IncludeStudios = true,
+                    SearchTerm = SearchText
+                };
+
+                try
+                {
+                    SearchResults.Clear();
+                    var search = await ApiClient.GetSearchHintsAsync(query);
+                    if (search != null && !search.SearchHints.IsNullOrEmpty())
+                    {
+                        SearchResults = search.SearchHints.Select(x => new SearchHintViewModel(Services, x)).ToObservableCollection();
+                        SetProgressBar(Resources.SysTraySearching);
+                    }
+                }
+                catch (HttpException e)
+                {
+
+                }
+                finally
+                {
+                    SetProgressBar();
+                }
+            }
+        }
+    }
+}
