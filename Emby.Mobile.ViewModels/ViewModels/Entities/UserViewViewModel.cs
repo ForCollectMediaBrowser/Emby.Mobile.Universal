219cfefbe2cb4a7dcc6240167077282d4870ed01
Scott Lovegrove <scottisafool@live.co.uk>
2015-09-04 23:25:50 +0100

    Basic card UI showing

--- Emby.Mobile.ViewModels/ViewModels/Entities/UserViewViewModel.cs
+++ Emby.Mobile.ViewModels/ViewModels/Entities/UserViewViewModel.cs
@@ -1,12 +1,24 @@
-﻿using Emby.Mobile.Core.Interfaces;
+﻿using System.Threading.Tasks;
+using Emby.Mobile.Core.Interfaces;
 using MediaBrowser.Model.Dto;
 
 namespace Emby.Mobile.ViewModels.Entities
 {
     public class UserViewViewModel : ItemViewModel
     {
+        private bool _cardLoaded;
+
         public UserViewViewModel(IServices services, BaseItemDto itemInfo) : base(services, itemInfo)
         {
+
+        }
+
+        private async Task GetCardData(bool isRefresh)
+        {
+            if (_cardLoaded && !isRefresh)
+            {
+                return;
+            }
         }
     }
 }
