2b10949ce44d04ec29fddcd6c178b951a6799ada
7illusions <z@7illusions.com>
2015-09-09 07:54:51 +0200

    Generate stream-info for player and report position

--- Emby.Mobile.Universal/Controls/MediaPlayers/MediaFoundationAudioPlayer.cs
+++ Emby.Mobile.Universal/Controls/MediaPlayers/MediaFoundationAudioPlayer.cs
@@ -9,12 +9,22 @@ using Windows.UI.Xaml;
 using Emby.Mobile.Universal.Services;
 using MediaBrowser.Model.Session;
 using MediaBrowser.Model.Dlna;
+using Emby.Mobile.Universal.Extensions;
+using GalaSoft.MvvmLight.Ioc;
+using MediaBrowser.Model.ApiClient;
+using MediaBrowser.ApiInteraction.Playback;
+using MediaBrowser.Model.MediaInfo;
+using MediaBrowser.ApiInteraction.Data;
+using System.IO;
+using MediaBrowser.Model.Net;
+using Emby.Mobile.Universal.Core.Helpers;
 
 namespace Emby.Mobile.Universal.Controls.MediaPlayers
 {
     [TemplatePart(Name = "Player", Type = typeof(MediaElement))]
     public class MediaFoundationAudioPlayer : Control, IMediaPlayer
     {
+        private DispatcherTimer _postionChangedTimer;
         private StreamInfo _streamInfo;
         private BaseItemDto _item;
         private MediaElement _player;
@@ -30,7 +40,25 @@ namespace Emby.Mobile.Universal.Controls.MediaPlayers
 
         public MediaFoundationAudioPlayer()
         {
+            _postionChangedTimer = new DispatcherTimer();
+            _postionChangedTimer.Interval = TimeSpan.FromSeconds(1);
+            _postionChangedTimer.Tick += PostionChangedTimer_Tick;
             DefaultStyleKey = typeof(MediaFoundationAudioPlayer);
+        }
+
+        private void PostionChangedTimer_Tick(object sender, object e)
+        {
+            AppServices.PlaybackService.ReportPlaybackProgress(new PlaybackProgressInfo
+            {
+                ItemId = _item.Id,
+                CanSeek = CanSeek,
+                AudioStreamIndex = null,
+                IsPaused = _player.PlaybackRate == 0,
+                IsMuted = _player.IsMuted,
+                VolumeLevel = Convert.ToInt32(_player.Volume),
+                PositionTicks = _player.Position.Ticks,
+                PlayMethod = _streamInfo.PlayMethod
+            }, _streamInfo);
         }
 
         protected override void OnApplyTemplate()
@@ -42,12 +70,19 @@ namespace Emby.Mobile.Universal.Controls.MediaPlayers
             _player.VolumeChanged += Player_VolumeChanged;
             _player.MediaOpened += Player_MediaOpened;
             _player.MediaEnded += Player_MediaEnded;
+            _player.MediaFailed += Player_MediaFailed;
 
             AppServices.PlaybackService.RegisterPlayer(this);
         }
 
+        private void Player_MediaFailed(object sender, ExceptionRoutedEventArgs e)
+        {
+            _postionChangedTimer.Stop();
+        }
+
         private void Player_MediaEnded(object sender, RoutedEventArgs e)
         {
+            _postionChangedTimer.Stop();
             AppServices.PlaybackService.ReportPlaybackStopped(new PlaybackStopInfo
             {
                 ItemId = _item.Id,
@@ -67,6 +102,7 @@ namespace Emby.Mobile.Universal.Controls.MediaPlayers
                 VolumeLevel = Convert.ToInt32(_player.Volume),
                 PositionTicks = _player.Position.Ticks
             });
+            _postionChangedTimer.Start();
         }
 
         private void Player_VolumeChanged(object sender, RoutedEventArgs e)
@@ -76,7 +112,7 @@ namespace Emby.Mobile.Universal.Controls.MediaPlayers
 
         private void Player_CurrentStateChanged(object sender, RoutedEventArgs e)
         {
-            
+          
         }
 
         public void DecreaseVolume()
@@ -99,12 +135,12 @@ namespace Emby.Mobile.Universal.Controls.MediaPlayers
 
         public Task Play(PlaylistItem item, double position = 0)
         {
-            throw new NotImplementedException();
+            return PlayItem(item, (int)position);
         }
 
         public Task Play(List<PlaylistItem> items, double position = 0)
         {
-            throw new NotImplementedException();
+            return PlayItem(items[0], (int)position);
         }
 
         public Task Add(List<PlaylistItem> items)
@@ -132,7 +168,7 @@ namespace Emby.Mobile.Universal.Controls.MediaPlayers
         }
 
         public Task<bool> SetNext()
-        {            
+        {
             throw new NotImplementedException();
         }
 
@@ -164,5 +200,49 @@ namespace Emby.Mobile.Universal.Controls.MediaPlayers
         public void NextSubtitleStream() { }
         public void SetAudioStreamIndex(int audioStreamIndex) { }
         public void SetSubtitleStreamIndex(int? subtitleStreamIndex) { }
+
+
+        private async Task PlayItem(PlaylistItem item, int positionTicks)
+        {
+            var client = SimpleIoc.Default.GetInstance<IConnectionManager>().GetApiClient(item.Item);
+            var profile = await ConnectionManagerFactory.GetProfileAsync();
+            _item = item.Item;
+            _streamInfo = await item.GetStreamInfoAsync(positionTicks,
+                                                   SimpleIoc.Default.GetInstance<IServerInfoService>().IsOffline,
+                                                   SimpleIoc.Default.GetInstance<IPlaybackManager>(),
+                                                   client,
+                                                   profile);
+
+            if (_streamInfo.PlayMethod == PlayMethod.DirectPlay && _streamInfo.MediaSource?.Protocol == MediaProtocol.File)
+            {
+                await SetLocalFileSourceAsync();
+            }
+            else
+            {
+                SetRemoteSource(client);
+                _player.Play();
+            }
+        }
+
+        private async Task SetLocalFileSourceAsync()
+        {
+            var assetManager = SimpleIoc.Default.GetInstance<ILocalAssetManager>();
+            try
+            {
+                var stream = await assetManager.GetFileStream(_streamInfo);
+                _player.SetSource(stream.AsRandomAccessStream(), MimeTypes.GetMimeType("." + _streamInfo.Container));
+            }
+            catch (Exception e)
+            {
+                //_log.Error("Failed to set local file Source", e);
+            }
+        }
+
+        private void SetRemoteSource(IApiClient client)
+        {
+            var url = _streamInfo.ToUrl(client.GetApiUrl("/"), client.AccessToken);
+            var source = new Uri(url);
+            _player.Source = source;
+        }
     }
 }
