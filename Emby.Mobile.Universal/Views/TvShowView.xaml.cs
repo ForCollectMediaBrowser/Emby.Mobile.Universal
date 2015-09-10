80a0cd76e6099604d2d4d3d91a5d2b3e4646117b
Scott Lovegrove <scottisafool@live.co.uk>
2015-09-10 22:20:27 +0100

    Added really basic UI

--- Emby.Mobile.Universal/Views/TvShowView.xaml.cs
+++ Emby.Mobile.Universal/Views/TvShowView.xaml.cs
@@ -10,6 +10,9 @@ namespace Emby.Mobile.Universal.Views
     /// </summary>
     public sealed partial class TvShowView : ICanHasHeaderMenu
     {
+        private IItemSettable Item => DataContext as IItemSettable;
+        private TvShowViewModel TvShow => DataContext as TvShowViewModel;
+
         public TvShowView()
         {
             InitializeComponent();
