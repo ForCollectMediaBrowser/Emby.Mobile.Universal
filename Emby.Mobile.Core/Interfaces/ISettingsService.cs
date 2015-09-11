aacb2d3f303b9dcfc4a25287f99d827dabab6287
Scott Lovegrove <scottisafool@live.co.uk>
2015-09-11 21:48:07 +0100

    Added SettingsService files

---
+++ Emby.Mobile.Core/Interfaces/ISettingsService.cs
@@ -0,0 +1,11 @@
+﻿namespace Emby.Mobile.Core.Interfaces
+{
+    public interface ISettingsService
+    {
+        bool ShowMissingEpisodes { get; set; }
+        bool ShowUnairedEpisodes { get; set; }
+        bool EnableImageEnhancers { get; set; }
+        ISettingsService Load();
+        void Save();
+    }
+}
---
+++ Emby.Mobile.Universal.Core/Services/SettingsService.cs
@@ -0,0 +1,33 @@
+﻿using Cimbalino.Toolkit.Services;
+using Emby.Mobile.Core.Extensions;
+using Emby.Mobile.Core.Interfaces;
+
+namespace Emby.Mobile.Universal.Core.Services
+{
+    public class SettingsService : ISettingsService
+    {
+        private const string SettingsServiceKey = "SettingsServiceKey";
+        private readonly IApplicationSettingsService _settings;
+        private readonly IApplicationSettingsServiceHandler _roamingSettings;
+
+        public SettingsService(IApplicationSettingsService settings)
+        {
+            _settings = settings;
+            _roamingSettings = settings.Roaming;
+        }
+
+        public bool ShowMissingEpisodes { get; set; }
+        public bool ShowUnairedEpisodes { get; set; }
+        public bool EnableImageEnhancers { get; set; }
+        public ISettingsService Load()
+        {
+            var settings = _roamingSettings.SafeGet<SettingsService>(SettingsServiceKey);
+            return settings;
+        }
+
+        public void Save()
+        {
+            _roamingSettings.SafeSet(SettingsServiceKey, this);
+        }
+    }
+}
