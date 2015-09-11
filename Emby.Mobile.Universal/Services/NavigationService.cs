536a8b5a788c6287fdc0583cf65b9554bae3fce7
Scott Lovegrove <scottisafool@live.co.uk>
2015-09-11 16:01:20 +0100

    Updated navigation service

--- Emby.Mobile.Universal/Services/NavigationService.cs
+++ Emby.Mobile.Universal/Services/NavigationService.cs
@@ -89,7 +89,7 @@ namespace Emby.Mobile.Universal.Services
 
         public bool NavigateToItem(BaseItemDto item)
         {
-            var value = false;
+            bool value;
 
             var type = item.Type.ToLower();
             if (type.Contains("collectionfolder")) type = "collectionfolder";
@@ -111,10 +111,11 @@ namespace Emby.Mobile.Universal.Services
                     value = Navigate<MovieView>(item);
                     break;
                 case "series":
-                    value = Navigate<TvShowView>();
+                    value = Navigate<TvShowView>(item);
                     break;
-                //    case "season":
-                //        break;
+                case "season":
+                    value = Navigate<SeasonView>(item);
+                    break;
                 //    case "episode":
                 //        break;
                 //    case "trailer":
