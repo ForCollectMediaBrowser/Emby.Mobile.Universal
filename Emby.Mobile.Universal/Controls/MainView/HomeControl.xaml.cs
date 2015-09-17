9871f5717dee369674605900168e0b3b21f3d351
7illusions <z@7illusions.com>
2015-09-17 21:17:25 +0200

    Added HomeControl

---
+++ Emby.Mobile.Universal/Controls/MainView/HomeControl.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Emby.Mobile.ViewModels;
+using Windows.UI.Xaml.Controls;
+
+namespace Emby.Mobile.Universal.Controls.MainView
+{
+    public sealed partial class HomeControl : UserControl
+    {        
+        public HomeControl()
+        {
+            InitializeComponent();
+            if (!ViewModelBase.IsInDesignModeStatic)
+            {
+                DataContextChanged += async (s, e) =>
+                {
+                    if (Home != null)
+                    {
+                        await Home.Refresh();
+                    }
+                };
+            }
+        }
+        
+
+        private HomeViewModel Home => DataContext as HomeViewModel;
+    }
+}
