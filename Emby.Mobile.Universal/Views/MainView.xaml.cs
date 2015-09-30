ecdf3985c8a81fd2ee5c1b03565e8422628c9d31
Scott Lovegrove <scottisafool@live.co.uk>
2015-09-30 18:33:03 +0100

    Added selected states to the home headers

--- Emby.Mobile.Universal/Views/MainView.xaml.cs
+++ Emby.Mobile.Universal/Views/MainView.xaml.cs
@@ -1,15 +1,34 @@
-﻿using Emby.Mobile.Universal.Interfaces;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Windows.UI.Xaml.Controls;
+using Emby.Mobile.Universal.Interfaces;
 using Emby.Mobile.ViewModels;
 
 namespace Emby.Mobile.Universal.Views
 {
     public sealed partial class MainView : ICanHasHeaderMenu
     {
+        private readonly List<string> HeaderList = new List<string>
+        {
+            Mobile.Core.Strings.Resources.HeaderHome,
+            Mobile.Core.Strings.Resources.HeaderWatchList,
+            Mobile.Core.Strings.Resources.HeaderFavourites
+        };
+
         public MainView()
         {
             InitializeComponent();
+            Loaded += (sender, args) =>
+            {
+                Header.SelectedItem = Header.Items.FirstOrDefault();
+            };
         }
 
         private MainViewModel Main => DataContext as MainViewModel;
+
+        private void Header_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Content.SelectedIndex = Header.SelectedIndex;
+        }
     }
 }
