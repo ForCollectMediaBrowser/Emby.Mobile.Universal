f6905d4f35f22512c1a1108b1f181bc63935e0e0
Scott Lovegrove <scottisafool@live.co.uk>
2015-10-02 17:45:55 +0100

    Added Textbox and passwordbox controls

---
+++ Emby.Mobile.Universal/Controls/EmbyPasswordBox.cs
@@ -0,0 +1,74 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Emby.Mobile.Universal.Controls
+{
+    [TemplatePart(Name = PasswordBox, Type = typeof(PasswordBox))]
+    [TemplateVisualState(GroupName = CommonStatesGroup, Name = FocusedState)]
+    [TemplateVisualState(GroupName = CommonStatesGroup, Name = LostFocusState)]
+    public sealed class EmbyPasswordBox : Control
+    {
+        private const string PasswordBox = "PasswordBox";
+        private const string CommonStatesGroup = "CommonStates";
+        private const string FocusedState = "Focused";
+        private const string LostFocusState = "LostFocus";
+
+        private PasswordBox _passwordBox;
+
+        public static readonly DependencyProperty PasswordProperty = DependencyProperty.Register(
+            "Password", typeof (string), typeof (EmbyPasswordBox), new PropertyMetadata(default(string)));
+
+        public string Password
+        {
+            get { return (string) GetValue(PasswordProperty); }
+            set { SetValue(PasswordProperty, value); }
+        }
+
+        public static readonly DependencyProperty PlaceholderTextProperty = DependencyProperty.Register(
+            "PlaceholderText", typeof (string), typeof (EmbyPasswordBox), new PropertyMetadata(default(string)));
+
+        public string PlaceholderText
+        {
+            get { return (string) GetValue(PlaceholderTextProperty); }
+            set { SetValue(PlaceholderTextProperty, value); }
+        }
+
+        public EmbyPasswordBox()
+        {
+            DefaultStyleKey = typeof(EmbyPasswordBox);
+        }
+        
+        protected override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            _passwordBox = GetTemplateChild(PasswordBox) as PasswordBox;
+
+            if (_passwordBox != null)
+            {
+                _passwordBox.PasswordChanged -= PasswordBoxOnPasswordChanged;
+                _passwordBox.PasswordChanged += PasswordBoxOnPasswordChanged;
+            }
+
+            LostFocus -= OnLostFocus;
+            LostFocus += OnLostFocus;
+
+            GotFocus -= OnGotFocus;
+            GotFocus += OnGotFocus;
+        }
+
+        private void OnGotFocus(object sender, RoutedEventArgs routedEventArgs)
+        {
+            VisualStateManager.GoToState(this, FocusedState, true);
+        }
+
+        private void PasswordBoxOnPasswordChanged(object sender, RoutedEventArgs e)
+        {
+            Password = _passwordBox.Password;
+        }
+
+        private void OnLostFocus(object sender, RoutedEventArgs routedEventArgs)
+        {
+            VisualStateManager.GoToState(this, LostFocusState, true);
+        }
+    }
+}
---
+++ Emby.Mobile.Universal/Controls/EmbyTextBox.cs
@@ -0,0 +1,30 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Animation;
+
+namespace Emby.Mobile.Universal.Controls
+{
+    public sealed class EmbyTextBox : TextBox
+    {
+        private Storyboard _storyboard;
+
+        public EmbyTextBox()
+        {
+            DefaultStyleKey = typeof(EmbyTextBox);
+        }
+
+        protected override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            _storyboard = GetTemplateChild("LostFocusStory") as Storyboard;
+
+            LostFocus -= OnLostFocus;
+            LostFocus += OnLostFocus;
+        }
+
+        private void OnLostFocus(object sender, RoutedEventArgs routedEventArgs)
+        {
+            _storyboard?.Begin();
+        }
+    }
+}
