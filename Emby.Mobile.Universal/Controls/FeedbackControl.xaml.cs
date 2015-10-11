81b3327a814bd21608f5638b992d0ed7e265adda
Scott Lovegrove <scottisafool@live.co.uk>
2015-10-11 09:46:22 +0100

    Added feedback button to header and control/vm

---
+++ Emby.Mobile.Universal/Controls/FeedbackControl.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Emby.Mobile.Universal.Controls
+{
+    public sealed partial class FeedbackControl
+    {
+        public FeedbackControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Emby.Mobile.ViewModels/ViewModels/FeedbackViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Emby.Mobile.Core.Interfaces;
+
+namespace Emby.Mobile.ViewModels
+{
+    public class FeedbackViewModel : ViewModelBase
+    {
+        public FeedbackViewModel(IServices services) : base(services)
+        {
+        }
+    }
+}
