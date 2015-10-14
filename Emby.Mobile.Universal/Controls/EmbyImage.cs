05d9d979d78cc65e79c1bcdcad4b8b029919fe0f
Scott Lovegrove <scottisafool@live.co.uk>
2015-10-14 18:40:24 +0100

    Add placeholders to EmbyImage

--- Emby.Mobile.Universal/Controls/EmbyImage.cs
+++ Emby.Mobile.Universal/Controls/EmbyImage.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media;
 
 namespace Emby.Mobile.Universal.Controls
 {
     [TemplatePart(Name = TheImage)]
+    [TemplatePart(Name = PlaceHolder)]
     [TemplateVisualState(GroupName = ImageLoadedGroup, Name = LoadedState)]
     [TemplateVisualState(GroupName = ImageLoadedGroup, Name = NotLoadedState)]
     public sealed class EmbyImage : Control
@@ -14,26 +14,82 @@ namespace Emby.Mobile.Universal.Controls
         private const string LoadedState = "Loaded";
         private const string NotLoadedState = "NotLoaded";
         private const string TheImage = "TheImage";
+        private const string PlaceHolder = "PlaceholderContent";
 
         // This may change to something else, but for now, just use an Image control in the template
         private Image _image;
+        private ContentPresenter _placeholder;
 
         public static readonly DependencyProperty StretchProperty = DependencyProperty.Register(
-            "Stretch", typeof (Stretch), typeof (EmbyImage), new PropertyMetadata(default(Stretch)));
+            "Stretch", typeof(Stretch), typeof(EmbyImage), new PropertyMetadata(default(Stretch)));
 
         public Stretch Stretch
         {
-            get { return (Stretch) GetValue(StretchProperty); }
+            get { return (Stretch)GetValue(StretchProperty); }
             set { SetValue(StretchProperty, value); }
         }
 
         public static readonly DependencyProperty SourceProperty = DependencyProperty.Register(
-            "Source", typeof (ImageSource), typeof (EmbyImage), new PropertyMetadata(default(ImageSource)));
+            "Source", typeof(ImageSource), typeof(EmbyImage), new PropertyMetadata(default(ImageSource)));
 
         public ImageSource Source
         {
-            get { return (ImageSource) GetValue(SourceProperty); }
+            get { return (ImageSource)GetValue(SourceProperty); }
             set { SetValue(SourceProperty, value); }
+        }
+
+        public static readonly DependencyProperty PlaceholderProperty = DependencyProperty.Register(
+            "Placeholder", typeof(object), typeof(EmbyImage), new PropertyMetadata(default(object)));
+
+        public object Placeholder
+        {
+            get { return GetValue(PlaceholderProperty); }
+            set { SetValue(PlaceholderProperty, value); }
+        }
+
+        public static readonly DependencyProperty PlaceholderTypeProperty = DependencyProperty.Register(
+            "PlaceholderType", typeof(PlaceholderType), typeof(EmbyImage), new PropertyMetadata(default(PlaceholderType), OnPlaceholderTypeChanged));
+
+        public PlaceholderType PlaceholderType
+        {
+            get { return (PlaceholderType)GetValue(PlaceholderTypeProperty); }
+            set { SetValue(PlaceholderTypeProperty, value); }
+        }
+
+        public static readonly DependencyProperty PlaceholderStretchProperty = DependencyProperty.Register(
+            "PlaceholderStretch", typeof(Stretch), typeof(EmbyImage), new PropertyMetadata(default(Stretch)));
+
+        public Stretch PlaceholderStretch
+        {
+            get { return (Stretch)GetValue(PlaceholderStretchProperty); }
+            set { SetValue(PlaceholderStretchProperty, value); }
+        }
+
+        public static readonly DependencyProperty ImageTemplateProperty = DependencyProperty.Register(
+            "ImageTemplate", typeof(DataTemplate), typeof(EmbyImage), new PropertyMetadata(default(DataTemplate)));
+
+        public DataTemplate ImageTemplate
+        {
+            get { return (DataTemplate)GetValue(ImageTemplateProperty); }
+            set { SetValue(ImageTemplateProperty, value); }
+        }
+
+        public static readonly DependencyProperty FontTemplateProperty = DependencyProperty.Register(
+            "FontTemplate", typeof(DataTemplate), typeof(EmbyImage), new PropertyMetadata(default(DataTemplate)));
+
+        public DataTemplate FontTemplate
+        {
+            get { return (DataTemplate)GetValue(FontTemplateProperty); }
+            set { SetValue(FontTemplateProperty, value); }
+        }
+
+        public static readonly DependencyProperty SymbolTemplateProperty = DependencyProperty.Register(
+            "SymbolTemplate", typeof(DataTemplate), typeof(EmbyImage), new PropertyMetadata(default(DataTemplate)));
+
+        public DataTemplate SymbolTemplate
+        {
+            get { return (DataTemplate)GetValue(SymbolTemplateProperty); }
+            set { SetValue(SymbolTemplateProperty, value); }
         }
 
         public EmbyImage()
@@ -45,6 +101,7 @@ namespace Emby.Mobile.Universal.Controls
         {
             base.OnApplyTemplate();
 
+            _placeholder = GetTemplateChild(PlaceHolder) as ContentPresenter;
             _image = GetTemplateChild(TheImage) as Image;
             if (_image != null)
             {
@@ -53,6 +110,34 @@ namespace Emby.Mobile.Universal.Controls
 
                 _image.ImageFailed -= ImageOnImageFailed;
                 _image.ImageFailed += ImageOnImageFailed;
+            }
+
+            SetPlaceholder();
+        }
+
+        private static void OnPlaceholderTypeChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            (sender as EmbyImage)?.SetPlaceholder();
+        }
+
+        private void SetPlaceholder()
+        {
+            if (_placeholder == null)
+            {
+                return;
+            }
+
+            switch (PlaceholderType)
+            {
+                case PlaceholderType.Font:
+                    _placeholder.ContentTemplate = FontTemplate;
+                    break;
+                case PlaceholderType.Image:
+                    _placeholder.ContentTemplate = ImageTemplate;
+                    break;
+                case PlaceholderType.Symbol:
+                    _placeholder.ContentTemplate = SymbolTemplate;
+                    break;
             }
         }
 
@@ -65,5 +150,12 @@ namespace Emby.Mobile.Universal.Controls
         {
             VisualStateManager.GoToState(this, LoadedState, true);
         }
+    }
+
+    public enum PlaceholderType
+    {
+        Image,
+        Font,
+        Symbol
     }
 }
