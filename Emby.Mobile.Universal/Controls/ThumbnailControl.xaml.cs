30f0b72813f3940991ec1af9bcb089977f36b89e
Scott Lovegrove <scottisafool@live.co.uk>
2015-10-15 18:08:11 +0100

    Extracted xaml from template, put in control

---
+++ Emby.Mobile.Universal/Controls/ThumbnailControl.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Windows.UI.Xaml;
+using Emby.Mobile.ViewModels.Entities;
+
+namespace Emby.Mobile.Universal.Controls
+{
+    public sealed partial class ThumbnailControl
+    {
+        private ItemViewModel Item => DataContext as ItemViewModel;
+
+        public static readonly DependencyProperty ImageUrlProperty = DependencyProperty.Register(
+            "ImageUrl", typeof (string), typeof (ThumbnailControl), new PropertyMetadata(default(string)));
+
+        public string ImageUrl
+        {
+            get { return (string) GetValue(ImageUrlProperty); }
+            set { SetValue(ImageUrlProperty, value); }
+        }
+
+        public ThumbnailControl()
+        {
+            InitializeComponent();
+            DataContextChanged += (sender, args) => Bindings.Update();
+        }
+    }
+}
